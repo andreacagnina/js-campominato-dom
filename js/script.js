16167a1544897619b582727e3c27465466e339de
andreacagnina <amirkal@libero.it>
2024-07-08 17:01:24 +0200

    [feat] add comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// una funzione che crea un quadrato e lo restituisce
 function CreaUnQuadrato() {
     let square = document.createElement('div');
     square.className = ('content width10');
     return square;
 }
 
+// una funzione con parametro che crea un array vuoto dove il numero massimo di elementi che contiene è 16. Tramite if il contenuto viene aggiunto all'array solo se il valore randomico generato non è già presente
 function createdBombs(max) {
     const bombsCreated = [];
 
@@ -18,7 +20,7 @@ function createdBombs(max) {
 }
 
 
-
+// Allo start ho un innerhtml che mi ripulisce il Contentenuto, un array vuoto in cui verrà iniettata la funzione alla quale parametro corrisponde alle variabili del numero di cell massime per livello. Come valore di default sono state date quelle del 1° livello che poi verrano sovrascritte dall'if che inizializza n° di celle e width
 document.getElementById('btn_start').addEventListener('click', function () {
     const contenitore = document.getElementById('container');
     contenitore.innerHTML = '';
@@ -26,23 +28,26 @@ document.getElementById('btn_start').addEventListener('click', function () {
 
     let livello = document.getElementById('level').value;
     console.log(livello)
+
+    // valore di defoault 1° livello, giusto per creare le variabili e non fare delle variabili 0 e un ulteriore if
     let maxCels = 100;
-    let grid = 'width10';
+    let col = 'width10';
 
     if (livello == 2) {
         maxCels = 81;
-        grid = 'width9';
+        col = 'width9';
+
     } else if (livello == 3) {
         maxCels = 49;
-        grid = 'width7';
+        col = 'width7';
     }
+    // l'array vuoto prende i valori dalla funzione e dalle variabili maxcels
     bombs = createdBombs(maxCels);
 
-
-
+    // Ciclo per creare tanti quadrati quante sono le MaxCels. Ogni quadrato sarà clickabile e prenderà una classe per colorarsi.
     for (let i = 1; i <= maxCels; i++) {
         let selSquare = CreaUnQuadrato();
-        selSquare.classList.add(grid);
+        selSquare.classList.add(col);
 
         selSquare.addEventListener('click', function () {
             this.className += (' bg_cel');
